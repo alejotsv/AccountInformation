5e751047c71d62e1508c6243ac771272d5dcd8f2
Alejandro Salas <asalas.wd@outlook.com>
2022-11-09 20:22:16 -0500

    Create main class

--- src/com/java21days/AccountInformation.java
+++ src/com/java21days/AccountInformation.java
@@ -1,9 +1,16 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AccountInformation extends JFrame {
+    Dimension dim = new Dimension(400, 450);
+
     AccountInformation(String title){
         super(title);
+        setSize(dim);
+
+
+        setVisible(true);
     }
 }
---
+++ src/com/java21days/SetPassword.java
@@ -0,0 +1,7 @@
+package com.java21days;
+
+public class SetPassword {
+    public static void main(String[] args) {
+        AccountInformation passwordWindow = new AccountInformation("Account Information");
+    }
+}
