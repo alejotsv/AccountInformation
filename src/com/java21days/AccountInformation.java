f94c8b545e87325066c1f9b2c0894399ed9a228b
Alejandro Salas <asalas.wd@outlook.com>
2022-11-09 20:40:51 -0500

    Create method to create labels from String array

--- src/com/java21days/AccountInformation.java
+++ src/com/java21days/AccountInformation.java
@@ -6,11 +6,11 @@ import java.awt.*;
 public class AccountInformation extends JFrame {
     Dimension dim = new Dimension(400, 450);
 
-    AccountInformation(String title){
+    AccountInformation(String title, JPanel panel){
         super(title);
         setSize(dim);
-
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(panel);
         setVisible(true);
     }
 }
--- src/com/java21days/SetPassword.java
+++ src/com/java21days/SetPassword.java
@@ -1,7 +1,23 @@
 package com.java21days;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class SetPassword {
     public static void main(String[] args) {
-        AccountInformation passwordWindow = new AccountInformation("Account Information");
+        JPanel panel = new JPanel();
+
+
+
+        AccountInformation passwordWindow = new AccountInformation("Account Information", panel);
+    }
+
+    ArrayList<JLabel> createLabels(String[] labels){
+        ArrayList<JLabel> jLabels = new ArrayList<>();
+        for(int i=0; i<labels.length; i++){
+            JLabel jl = new JLabel(labels[i]);
+            jLabels.add(jl);
+        }
+        return jLabels;
     }
 }
