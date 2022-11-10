b8e4888a2f57c101d356c4e944e5e4959b05b298
Alejandro Salas <asalas.wd@outlook.com>
2022-11-09 22:04:04 -0500

    Fix layout by updating textarea and textfield length

--- src/com/java21days/SetPassword.java
+++ src/com/java21days/SetPassword.java
@@ -8,13 +8,20 @@ public class SetPassword {
         JPanel panel = new JPanel();
         String[] labels = { "Username: ", "Password: ", "Comments: " };
         ArrayList<JLabel> jLabels = createLabels(labels);
-        JTextField username = new JTextField(12);
-        JPasswordField password = new JPasswordField(12);
+        JTextField username = new JTextField(15);
+        JPasswordField password = new JPasswordField(15);
+        JTextArea textArea = new JTextArea(4, 15);
+        JButton ok = new JButton("OK");
+        JButton cancel = new JButton("Cancel");
 
         panel.add(jLabels.get(0));
         panel.add(username);
         panel.add(jLabels.get(1));
         panel.add(password);
+        panel.add(jLabels.get(2));
+        panel.add(textArea);
+        panel.add(ok);
+        panel.add(cancel);
 
         AccountInformation passwordWindow = new AccountInformation("Account Information", panel);
     }
